8a70b43a941e30c211ebe6a81e97c3486ab38a15
rahulgogia65 <rahulgogia43@gmail.com>
2023-06-22 00:50:55 +0530

    Fix feature add friend in modal

--- lib/elixir_splitwise_web/live/dashboard/modals/add_friend.ex
+++ lib/elixir_splitwise_web/live/dashboard/modals/add_friend.ex
@@ -1,12 +1,7 @@
 defmodule ElixirSplitwiseWeb.Live.Dashboard.Modals.AddFriend do
   use ElixirSplitwiseWeb, :live_view
-  alias ElixirSplitwise.{Accounts, Accounts.User}
 
-  def mount(params, _session, socket) do
-    {:ok, assign(socket, form: params)}
+  def mount(_params, _session, socket) do
+    {:ok, assign(socket, form: to_form(%{}))}
   end
-
-  # def handle_call("submitted", params, socket) do
-  #   {:noreply, assign(socket, form: to_form(params))}
-  # end
 end
